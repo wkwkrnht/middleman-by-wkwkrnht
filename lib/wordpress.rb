d1703ba5adfe57c95cfccadd17ec2e7da8321ab2
wkwkrnht <wkwkrnht@gmail.com>
2018-02-08 22:44:19 +0900

    fix

--- lib/wordpress.rb
+++ lib/wordpress.rb
@@ -3,7 +3,7 @@ class WordPress
     require 'json'
 
     def initialize(uri)
-        HTTParty.base_uri uri
+        api_uri = uri
     end
 
     def posts
@@ -13,21 +13,21 @@ class WordPress
     protected
 
     def get_posts
-        tmp_json = HTTParty.get('/posts?type=post')
+        tmp_json = HTTParty.get(api_uri + '/posts?type=post')
         return JSON.parse(tmp_json)
     end
 
     def get_and_parse_info(id, type)
         if type == 'media'
-            tmp_json = HTTParty.get("/media?include=#{id}")
+            tmp_json = HTTParty.get(api_uri + "/media?include=#{id}")
             tmp_json = JSON.parse(tmp_json)
             return tmp_json['source_url']
         elsif type == 'author'
-            tmp_json = HTTParty.get("/users?include=#{id}")
+            tmp_json = HTTParty.get(api_uri + "/users?include=#{id}")
             tmp_json = JSON.parse(tmp_json)
             return tmp_json['name']
         elsif type == 'tag'
-            tmp_json = HTTParty.get("/tags?include=#{id}")
+            tmp_json = HTTParty.get(api_uri + "/tags?include=#{id}")
             tmp_json = JSON.parse(tmp_json)
             return tmp_json['name']
         end
