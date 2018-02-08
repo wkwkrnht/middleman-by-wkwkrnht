afc100bfb7b5476f21137241da103e846f8dd1c6
wkwkrnht <wkwkrnht@gmail.com>
2018-02-08 22:53:22 +0900

    fix

--- config.rb
+++ config.rb
@@ -1,9 +1,36 @@
 require 'ansi/code'
-require 'lib/wordpress'
+require 'httparty'
+require 'json'
 require 'slim'
 
-wordpress = WordPress.new('http://wkwkrnht.wp.xdomain.jp/wp-json')
-wordpress.posts.each do |post|
+api_uri = data.site.api_uri
+
+def wp_posts
+    @posts ||= get_posts
+end
+
+def get_posts
+    tmp_json = HTTParty.get(api_uri + '/posts?type=post')
+    return JSON.parse(tmp_json)
+end
+
+def get_and_parse_info(id, type)
+    if type == 'media'
+        tmp_json = HTTParty.get(api_uri + "/media?include=#{id}")
+        tmp_json = JSON.parse(tmp_json)
+        return tmp_json['source_url']
+    elsif type == 'author'
+        tmp_json = HTTParty.get(api_uri + "/users?include=#{id}")
+        tmp_json = JSON.parse(tmp_json)
+        return tmp_json['name']
+    elsif type == 'tag'
+        tmp_json = HTTParty.get(api_uri + "/tags?include=#{id}")
+        tmp_json = JSON.parse(tmp_json)
+        return tmp_json['name']
+    end
+end
+
+wp_posts.each do |post|
     proxy "/posts/#{post['slug']}/", "templates/post", locals: { post: post }
 end
 
