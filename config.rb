00f5e3c86bba13c30a2de52274f59225c8f4288f
wkwkrnht <wkwkrnht@gmail.com>
2018-02-08 23:25:09 +0900

    fix

--- config.rb
+++ config.rb
@@ -39,21 +39,21 @@ end
 
 def get_posts(api_uri)
     tmp_json = HTTParty.get(api_uri + '/posts?type=post')
-    return JSON.parse(tmp_json)
+    return JSON.parse(tmp_json.body)
 end
 
 def get_and_parse_info(api_uri, id, type)
     if type == 'media'
         tmp_json = HTTParty.get(api_uri + "/media?include=#{id}")
-        tmp_json = JSON.parse(tmp_json)
+        tmp_json = JSON.parse(tmp_json.body)
         return tmp_json['source_url']
     elsif type == 'author'
         tmp_json = HTTParty.get(api_uri + "/users?include=#{id}")
-        tmp_json = JSON.parse(tmp_json)
+        tmp_json = JSON.parse(tmp_json.body)
         return tmp_json['name']
     elsif type == 'tag'
         tmp_json = HTTParty.get(api_uri + "/tags?include=#{id}")
-        tmp_json = JSON.parse(tmp_json)
+        tmp_json = JSON.parse(tmp_json.body)
         return tmp_json['name']
     end
 end
